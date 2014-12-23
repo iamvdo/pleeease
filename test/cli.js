c91f2b48c3ced252bd9ae30e3a2f4b3b6c877cd7
Vincent De Oliveira <vincent@iamvdo.me>
2014-12-23 12:08:24 +0100

    Clean-up

--- test/cli.js
+++ test/cli.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var fs         = require('fs');
-var path       = require('path');
-var assert     = require('assert');
-var exec       = require('child_process').exec;
-var Options    = require('../lib/options');
+var CLI        = require('../lib/cli'),
+    Options    = require('../lib/options'),
+    Logger     = require('../lib/logger');
+
+var fs         = require('fs'),
+    path       = require('path'),
+    exec       = require('child_process').exec,
+    trim       = require('cli-color/trim'),
+    assert     = require('assert');
+
 var bin        = 'node ' + path.resolve(__dirname, '../bin/pleeease');
 var readFile   = require('../test/_helpers.js').readFile;
 var removeFile = require('../test/_helpers.js').removeFile;
-var CLI = require('../lib/cli');
 
 var __dirname__ = 'test/cli/';
 var __in__      = __dirname__ + 'in.css';
@@ -28,7 +32,7 @@ describe('CLI', function () {
 
     cli     = new CLI();
     options = new Options().options;
-    remove = true;
+    remove  = true;
 
   });
 
@@ -39,91 +43,76 @@ describe('CLI', function () {
   });
 
   it('should extend options', function(done) {
-
     cli.options.should.eql(options);
-
     remove = false;
-
     done();
-
   });
 
   it('should read from input file and write to output file', function(done) {
-
     exec(bin + ' compile '+ __in__ + ' to '+ __out__, function (err, stdout) {
       if (err) return done(err);
-
       var input  = readFile(__in__);
       var output = readFile(__out__);
-
       assert.equal(output, input);
-
       done();
-
     });
-
   });
 
   it('should read directory of inputs files and write to output file', function(done) {
-
     exec(bin + ' compile test/cli to '+ __out__, function (err, stdout) {
       if (err) return done(err);
-
       // there's only one file in test/cli, so read it
       var input  = readFile(__in__);
       var output = readFile(__out__);
-
       assert.equal(output, input);
-
       done();
-
     });
-
   });
 
   it('should read from input file and write to default output file if omitted', function(done) {
-
     exec(bin + ' compile '+ __in__, function (err, stdout) {
       if (err) return done(err);
-
       var input  = readFile(__in__);
       var output = readFile(options.out);
-
       assert.equal(output, input);
-
       // remove default output file
       out = options.out;
-
       done();
-
     });
-
   });
 
   it('should read from .pleeeaserc file if input and/or output files are omitted', function(done) {
-
     // create a .pleeeaserc file
     var json = '{"in": ["in.css"], "out": "out"}';
     var pleeeaseRC = fs.writeFileSync(__dirname__ + '.pleeeaserc', json);
-
     exec('cd ' + __dirname__ + ' && node ../../bin/pleeease compile', function (err, stdout) {
       if (err) return done(err);
-
       var input  = readFile(__in__);
       var output = readFile(__out__);
-
       assert.equal(output, input);
-
       // remove .pleeeaserc file
       removeFile(__dirname__ + '.pleeeaserc');
-
       // remove output file
       out = __out__;
-
       done();
-
     });
+  });
 
+  it('should return error when no files are found', function (done) {
+    remove = false;
+    exec(bin + ' compile ' + __dirname__ + 'not-found.css', function (err, stdout, stderr) {
+      assert(stderr.indexOf('File(s) not found') !== -1);
+      done();
+      }
+    );
+  });
+
+  it('should return success message when compile', function (done) {
+    exec(bin + ' compile '+ __in__ + ' to '+ __out__, function (err, stdout) {
+      if (err) return done(err);
+      assert(trim(stdout).search(/^Pleeease Compile [0-9]+ file/) !== -1);
+      done();
+    });
   });
 
 });
