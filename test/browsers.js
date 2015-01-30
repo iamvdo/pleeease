877b81cb03bae5c6f18cb8a55482cde3f5851f74
Vincent De Oliveira <vincent@iamvdo.me>
2015-01-30 11:40:45 +0100

    Add tests for Browsers

--- test/browsers.js
+++ test/browsers.js
@@ -7,19 +7,30 @@
  */
 describe('Browsers', function () {
 
-  var Browsers  = require('../lib/browsers');
-  var assert   = require('assert');
+  var Browsers = require('../lib/browsers');
 
   var browser;
 
-  beforeEach(function() {
+  beforeEach(function () {
   });
 
-  it('should create features', function () {
+  it('uses browserslist', function () {
+    browser = new Browsers({browsers: ['ie < 9']});
+    browser.selected.should.be.instanceOf(Array);
+    browser.selected[0].should.be.eql('ie 8');
+  });
 
-    browser = new Browsers({browsers: ['ie 8']});
+  it('uses default browsers when no options are set', function () {
+    browser = new Browsers();
+    browser.selected.should.be.instanceOf(Array);
+  });
+
+  it('creates features', function () {
+    browser = new Browsers();
+    browser.features.should.instanceOf(Object);
     browser.features.should.have.property('rem');
-
+    browser.features.should.have.property('opacity');
+    browser.features.should.have.property('pseudoElements');
   });
 
 });
